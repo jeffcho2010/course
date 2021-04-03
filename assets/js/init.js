37a2c2b82d386551b62b747044861e78d2e9e9a1
Jeff Cho <jeffcho2010@gmail.com>
2021-04-03 13:01:09 +0800

    deploy

---
+++ assets/js/init.js
@@ -0,0 +1,47 @@
+// Reveal.initialize({
+// 	controls: true,
+// 	progress: true,
+// 	history: true,
+// 	center: true,
+// 	width: 1600,
+// 	height: "100%",
+// 	transition: "slide",
+// 	dependencies: [{
+// 			src: "assets/lib/js/classList.js",
+// 			condition: function() {
+// 				return !document.body.classList;
+// 			},
+// 		},
+// 		{
+// 			src: "assets/plugin/markdown/marked.js",
+// 			condition: function() {
+// 				return !!document.querySelector("[data-markdown]");
+// 			},
+// 		},
+// 		{
+// 			src: "assets/plugin/markdown/markdown.js",
+// 			condition: function() {
+// 				return !!document.querySelector("[data-markdown]");
+// 			},
+// 		},
+// 		{
+// 			src: "assets/plugin/highlight/highlight.js",
+// 			async: true,
+// 			callback: function() {
+// 				hljs.initHighlightingOnLoad();
+// 			},
+// 		},
+// 		{
+// 		    src: "assets/plugin/search/search.js",
+// 		    async: true
+// 		},
+// 		{
+// 		    src: "assets/plugin/zoom-js/zoom.js",
+// 		    async: true
+// 		},
+// 		{
+// 		    src: "assets/plugin/notes/notes.js",
+// 		    async: true
+// 		},
+// 	],
+// });
